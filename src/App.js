41801b2747ed94c2175ebd2ef94b151398631017
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2025-02-05 18:11:46 +0530

    add vite base

--- src/App.js
+++ src/App.js
@@ -1,59 +1,31 @@
-class EventEmitter {
-  constructor() {
-    this.events = {};
-  }
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-  /**
-   * @param {string} eventName
-   * @param {Function} callback
-   * @return {Object}
-   */
-  subscribe(eventName, callback) {
-    if (eventName in this.events) {
-      this.events[eventName].push(callback);
-    } else {
-      this.events[eventName] = [callback];
-    }
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.name),
+    React.createElement("p", {}, props.description),
+  ]);
+};
 
-    return {
-      unsubscribe: () => {
-        this.events[eventName] = this.events[eventName].filter(
-          (cb) => cb !== callback,
-        );
-        return;
-      },
-    };
-  }
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Padre Gino's"),
+    React.createElement(Pizza, {
+      name: "The Pepperoni Pizza",
+      description: "Mozzarella Cheese, Pepperoni",
+    }),
+    React.createElement(Pizza, {
+      name: "The Hawaiian Pizza",
+      description: "Sliced Ham, Pineapple, Mozzarella Cheese",
+    }),
+    React.createElement(Pizza, {
+      name: "The Big Meat Pizza",
+      description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
+    }),
+  ]);
+};
 
-  /**
-   * @param {string} eventName
-   * @param {Array} args
-   * @return {Array}
-   */
-  emit(eventName, args = []) {
-    if (!this.events[eventName]) return [];
-
-    return this.events[eventName].map((event) => event(...args));
-  }
-}
-
-const emitter = new EventEmitter();
-
-// Subscribe to the onClick event with onClickCallback
-function onClickCallback1() {
-  return 99;
-}
-function onClickCallback2() {
-  return 45;
-}
-
-const sub1 = emitter.subscribe("onClick", onClickCallback1);
-const sub2 = emitter.subscribe("onClick", onClickCallback2);
-
-emitter;
-
-// let e1 = emitter.emit('onClick');
-sub1.unsubscribe();
-sub2.unsubscribe();
-
-console.log(emitter.emit("onClick"));
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(React.createElement(App));
