aedb4fc2e479956375e2d85135c41db26c306ed7
Dheeraj Purohit <purohitdheeraj89@gmail.com>
2025-02-05 18:41:36 +0530

    vite basic template

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,11 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 
-const Pizza = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("p", {}, props.description),
-  ]);
-};
-
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Padre Gino's"),
-    React.createElement(Pizza, {
-
-
-
-
-      name: "The Pepperoni Pizza",
-      description: "Mozzarella Cheese, Pepperoni",
-    }),
-    React.createElement(Pizza, {
-      name: "The Hawaiian Pizza",
-      description: "Sliced Ham, Pineapple, Mozzarella Cheese",
-    }),
-    React.createElement(Pizza, {
-      name: "The Big Meat Pizza",
-      description: "Bacon, Pepperoni, Italian Sausage, Chorizo Sausage",
-    }),
-  ]);
+  return (
+    <div>Hare krishna vite app running successfully!!</div>
+  );
 };
 
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App));
+root.render(<App />);
